098568130fd8af23d16660f5509f664bc8733ea5
Iain <Iain@DESKTOP-O9UC60V>
2022-05-29 14:43:11 -0400

    working score updater

--- game/main.cpp
+++ game/main.cpp
@@ -11,10 +11,17 @@ void setup()
 
 int score = 0;
 
+float acc = 0.f;
 bool loop()
 {
 	Time::UpdateTime();
-	if (Time::DeltaTime() > 2) score += get_rand(100);
+	acc += Time::DeltaTime();
+	if (acc > 2)
+	{
+		acc = 0;
+		score += get_rand(100);
+		printf("current score %d\n", score);
+	}
 
 	// update level system based on score?
 
