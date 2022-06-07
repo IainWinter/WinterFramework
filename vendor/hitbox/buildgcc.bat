d3935f7d0c1415d9da51d425f2ec5de0c9dac4cf
Iain <Iain@DESKTOP-O9UC60V>
2022-06-07 12:53:02 -0400

    removed gccbuild because premake for makefile works really well actually. and it also uses g++

--- vendor/hitbox/buildgcc.bat
+++
@@ -1,5 +0,0 @@
-mkdir lib
-cd lib
-g++ -I../include -I../../glm/include ../src/*.cpp -c
-ar rvs libhitbox.a ./*.o
-del *.o
--- vendor/imgui/gccbuild.bat
+++
@@ -1,9 +0,0 @@
-mkdir lib
-cd lib
-g++ -I../../sdl/include/SDL2 -I../include/imgui -I../src ../src/*.cpp -c
-ar rvs libimgui.a ./*.o
-del *.o
-
-:: del ../include/*impl*.h
-:: copy ..\backends\imgui_impl_sdl.h ..\include\imgui
-:: copy ..\backends\imgui_impl_opengl3.h ..\include\imgui
