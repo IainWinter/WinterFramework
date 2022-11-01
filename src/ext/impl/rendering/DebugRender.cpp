40cafb8dbb6bdf021131fd392fd05db81ed4a300
Iain <Iain@DESKTOP-O9UC60V>
2022-11-01 17:34:58 -0400

    moved fmod dlls for mac

--- src/ext/impl/rendering/DebugRender.cpp
+++ src/ext/impl/rendering/DebugRender.cpp
@@ -5,27 +5,27 @@ namespace Debug
 	BatchLineRenderer debugLines;
     BatchSpriteRenderer debugSprites;
 
-	void Begin()
-	{
-		debugLines.Begin();
+    void Begin()
+    {
+	    debugLines.Begin();
         debugSprites.Begin();
-	}
+    }
 
-	void End(const Camera& camera)
-	{
-		debugLines.Draw(camera);
+    void End(const Camera& camera)
+    {
+	    debugLines.Draw(camera);
         debugSprites.Draw(camera);
-	}
+    }
 
-	void Line(const vec2& a, const vec2& b, const Color& color)
-	{
-		debugLines.SubmitLine(a, b, color);
-	}
+    void Line(const vec2& a, const vec2& b, const Color& color)
+    {
+	    debugLines.SubmitLine(a, b, color);
+    }
 
-	void Ray(const vec2& a, const vec2& dist, const Color& color, float scale)
-	{
-		debugLines.SubmitLine(a, a + dist * scale, color);
-	}
+    void Ray(const vec2& a, const vec2& dist, const Color& color, float scale)
+    {
+	    debugLines.SubmitLine(a, a + dist * scale, color);
+    }
 
     void Box(const vec2& a, const vec2& b, const Color& color)
     {
