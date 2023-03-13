6b4df19edea22ce62d61d6093856d41f6f4d4b54
Iain <Iain@DESKTOP-O9UC60V>
2023-03-12 20:22:02 -0400

    added flag for lerp function on linux being already defined?

--- include/util/math.h
+++ include/util/math.h
@@ -23,7 +23,10 @@ vec2  get_randnc(float scale);
 
 vec2 get_rand_jitter(vec2 x, float jitter);
 
+#ifndef LINUX
 float lerp(float        a, float        b, float w);
+#endif
+
 vec2  lerp(const vec2&  a, const vec2&  b, float w);
 vec3  lerp(const vec3&  a, const vec3&  b, float w);
 vec4  lerp(const vec4&  a, const vec4&  b, float w);
